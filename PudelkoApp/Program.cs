448c3d7778f38f8fd8ef374a6ae8f72018413ea4
Cebix90 <79079434+Cebix90@users.noreply.github.com>
2023-04-16 15:59:09 +0200

    Dodaj pliki projektów.

---
+++ PudelkoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using PudelkoLibrary;
+
+namespace PudelkoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ PudelkoLibrary/Pudelko.cs
@@ -0,0 +1,19 @@
+﻿namespace PudelkoLibrary
+{
+    public class Pudelko
+    {
+
+        public double A { get; init; }
+        public double B { get; init; }
+        public double C { get; init; }
+        public UnitOfMeasure Unit { get; init; }
+
+        public Pudelko(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
+    
